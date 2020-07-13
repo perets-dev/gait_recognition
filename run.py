f2e2e533bad3f44008fd3a8915436988d0159742
perets-dev <perets.developer@gmail.com>
2020-07-13 16:39:35 +0300

    adding source code

--- run.py
+++ run.py
@@ -1,16 +1,17 @@
 import argparse
 import logging
-import sys
 import time
 
-from tf_pose import common
+import common
 import cv2
 import numpy as np
 from tf_pose.estimator import TfPoseEstimator
 from tf_pose.networks import get_graph_path, model_wh
 
-logger = logging.getLogger('TfPoseEstimatorRun')
-logger.handlers.clear()
+from lifting.prob_model import Prob3dPose
+from lifting.draw import plot_pose
+
+logger = logging.getLogger('TfPoseEstimator')
 logger.setLevel(logging.DEBUG)
 ch = logging.StreamHandler()
 ch.setLevel(logging.DEBUG)
@@ -21,72 +22,88 @@ logger.addHandler(ch)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='tf-pose-estimation run')
-    parser.add_argument('--image', type=str, default='./images/p1.jpg')
-    parser.add_argument('--model', type=str, default='cmu',
-                        help='cmu / mobilenet_thin / mobilenet_v2_large / mobilenet_v2_small')
-    parser.add_argument('--resize', type=str, default='0x0',
-                        help='if provided, resize images before they are processed. '
-                             'default=0x0, Recommends : 432x368 or 656x368 or 1312x736 ')
-    parser.add_argument('--resize-out-ratio', type=float, default=4.0,
-                        help='if provided, resize heatmaps before they are post-processed. default=1.0')
-
+    # parser.add_argument('--image', type=str, default='/Users/ildoonet/Downloads/me.jpg')
+    parser.add_argument('--image', type=str, default='./images/apink2.jpg')
+    # parser.add_argument('--model', type=str, default='mobilenet_320x240', help='cmu / mobilenet_320x240')
+    parser.add_argument('--model', type=str, default='mobilenet_thin_432x368', help='cmu_640x480 / cmu_640x360 / mobilenet_thin_432x368')
+    parser.add_argument('--scales', type=str, default='[None]', help='for multiple scales, eg. [1.0, (1.1, 0.05)]')
     args = parser.parse_args()
+    #scales = ast.literal_eval(scales)
 
-    w, h = model_wh(args.resize)
-    if w == 0 or h == 0:
-        e = TfPoseEstimator(get_graph_path(args.model), target_size=(432, 368))
-    else:
-        e = TfPoseEstimator(get_graph_path(args.model), target_size=(w, h))
+    w, h = model_wh(args.model)
+    e = TfPoseEstimator(get_graph_path(args.model), target_size=(w, h))
 
     # estimate human poses from a single image !
     image = common.read_imgfile(args.image, None, None)
-    if image is None:
-        logger.error('Image can not be read, path=%s' % args.image)
-        sys.exit(-1)
-
+    # image = cv2.fastNlMeansDenoisingColored(image, None, 10, 10, 7, 21)
     t = time.time()
-    humans = e.inference(image, resize_to_default=(w > 0 and h > 0), upsample_size=args.resize_out_ratio)
+    humans = e.inference(image, scales=[None])
+    # humans = e.inference(image, scales=[None, (0.7, 0.5, 1.8)])
+    # humans = e.inference(image, scales=[(1.2, 0.05)])
+    # humans = e.inference(image, scales=[(0.2, 0.2, 1.4)])
     elapsed = time.time() - t
 
     logger.info('inference image: %s in %.4f seconds.' % (args.image, elapsed))
 
+    image = cv2.imread(args.image, cv2.IMREAD_COLOR)
     image = TfPoseEstimator.draw_humans(image, humans, imgcopy=False)
+    cv2.imshow('tf-pose-estimation result', image)
+    cv2.waitKey()
 
-    try:
-        import matplotlib.pyplot as plt
+    #import sys
+    #sys.exit(0)
 
-        fig = plt.figure()
-        a = fig.add_subplot(2, 2, 1)
-        a.set_title('Result')
-        plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+    logger.info('3d lifting initialization.')
+    poseLifting = Prob3dPose('./src/lifting/models/prob_model_params.mat')
 
-        bgimg = cv2.cvtColor(image.astype(np.uint8), cv2.COLOR_BGR2RGB)
-        bgimg = cv2.resize(bgimg, (e.heatMat.shape[1], e.heatMat.shape[0]), interpolation=cv2.INTER_AREA)
+    image_h, image_w = image.shape[:2]
+    standard_w = 640
+    standard_h = 480
 
-        # show network output
-        a = fig.add_subplot(2, 2, 2)
-        plt.imshow(bgimg, alpha=0.5)
-        tmp = np.amax(e.heatMat[:, :, :-1], axis=2)
-        plt.imshow(tmp, cmap=plt.cm.gray, alpha=0.5)
-        plt.colorbar()
+    pose_2d_mpiis = []
+    visibilities = []
+    for human in humans:
+        pose_2d_mpii, visibility = common.MPIIPart.from_coco(human)
+        pose_2d_mpiis.append([(int(x * standard_w + 0.5), int(y * standard_h + 0.5)) for x, y in pose_2d_mpii])
+        visibilities.append(visibility)
 
-        tmp2 = e.pafMat.transpose((2, 0, 1))
-        tmp2_odd = np.amax(np.absolute(tmp2[::2, :, :]), axis=0)
-        tmp2_even = np.amax(np.absolute(tmp2[1::2, :, :]), axis=0)
+    pose_2d_mpiis = np.array(pose_2d_mpiis)
+    visibilities = np.array(visibilities)
+    transformed_pose2d, weights = poseLifting.transform_joints(pose_2d_mpiis, visibilities)
+    pose_3d = poseLifting.compute_3d(transformed_pose2d, weights)
 
-        a = fig.add_subplot(2, 2, 3)
-        a.set_title('Vectormap-x')
-        # plt.imshow(CocoPose.get_bgimg(inp, target_size=(vectmap.shape[1], vectmap.shape[0])), alpha=0.5)
-        plt.imshow(tmp2_odd, cmap=plt.cm.gray, alpha=0.5)
-        plt.colorbar()
+    import matplotlib.pyplot as plt
 
-        a = fig.add_subplot(2, 2, 4)
-        a.set_title('Vectormap-y')
-        # plt.imshow(CocoPose.get_bgimg(inp, target_size=(vectmap.shape[1], vectmap.shape[0])), alpha=0.5)
-        plt.imshow(tmp2_even, cmap=plt.cm.gray, alpha=0.5)
-        plt.colorbar()
-        plt.show()
-    except Exception as e:
-        logger.warning('matplitlib error, %s' % e)
-        cv2.imshow('result', image)
-        cv2.waitKey()
+    fig = plt.figure()
+    a = fig.add_subplot(2, 2, 1)
+    a.set_title('Result')
+    plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+
+    # show network output
+    a = fig.add_subplot(2, 2, 2)
+    # plt.imshow(CocoPose.get_bgimg(inp, target_size=(heatmap.shape[1], heatmap.shape[0])), alpha=0.5)
+    tmp = np.amax(e.heatMat, axis=2)
+    plt.imshow(tmp, cmap=plt.cm.gray, alpha=0.5)
+    plt.colorbar()
+
+    tmp2 = e.pafMat.transpose((2, 0, 1))
+    tmp2_odd = np.amax(np.absolute(tmp2[::2, :, :]), axis=0)
+    tmp2_even = np.amax(np.absolute(tmp2[1::2, :, :]), axis=0)
+
+    a = fig.add_subplot(2, 2, 3)
+    a.set_title('Vectormap-x')
+    # plt.imshow(CocoPose.get_bgimg(inp, target_size=(vectmap.shape[1], vectmap.shape[0])), alpha=0.5)
+    plt.imshow(tmp2_odd, cmap=plt.cm.gray, alpha=0.5)
+    plt.colorbar()
+
+    a = fig.add_subplot(2, 2, 4)
+    a.set_title('Vectormap-y')
+    # plt.imshow(CocoPose.get_bgimg(inp, target_size=(vectmap.shape[1], vectmap.shape[0])), alpha=0.5)
+    plt.imshow(tmp2_even, cmap=plt.cm.gray, alpha=0.5)
+    plt.colorbar()
+
+    for i, single_3d in enumerate(pose_3d):
+        plot_pose(single_3d)
+    plt.show()
+
+    pass
